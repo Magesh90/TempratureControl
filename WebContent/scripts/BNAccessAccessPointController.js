9d0cfde4c2e6493d3c77b5cf24b8064c44b864c7
Magesh90 <mageshb@live.com>
2016-09-19 16:10:25 -0500

    service variable

--- WebContent/scripts/BNAccessAccessPointController.js
+++ WebContent/scripts/BNAccessAccessPointController.js
@@ -1,9 +1,10 @@
-var BNAccessAccessPointController = function($scope) {
+var BNAccessAccessPointController = function($scope,accessPointService) {
 	$scope.title = "BNAccess Access Points Page";
 	$scope.homeScreeMessage = "AccessPoints are a holding container for WaSP Devices in the system. "
 			+ "WaSP are used to transmit wireless sensor data over the internet to Bluenica servers. "
 			+ "All data from a WasP Device is assigned to the AccessPoint containing it.";
 	var imageUrl = "/img/Early_icon_Sensor_Upgrade.png";
+	$scope.accessPointService=accessPointService;
 	$scope.accessPoints = [
 			{
 				name : "TCS Access Point I",
--- WebContent/scripts/BNAccessSelectedAPointController.js
+++ WebContent/scripts/BNAccessSelectedAPointController.js
@@ -1,3 +1,6 @@
-var BNAccessSelectedAPointController=function($scope){
-	
+var BNAccessSelectedAPointController=function($scope,accessPointService,$log){
+	$scope.accessPointService=accessPointService;
+	$log.info(accessPointService.accessPointName);
 }
+
+BNAccessHome.controller("BNAccessSelectedAPointController",BNAccessSelectedAPointController);
