94d25e2de7b715ced09e22f6a521c69df529b726
azarkabeer <azarkabeer@gmail.com>
2016-09-21 11:00:34 -0500

    Pushing from azar desktop

--- WebContent/scripts/BNAccessSelectedAPointController.js
+++ WebContent/scripts/BNAccessSelectedAPointController.js
@@ -3,23 +3,33 @@ var BNAccessSelectedAPointController = function($scope, accessPointService,
 	$scope.accessPointService = accessPointService;
 	$log.info($scope.accessPointService.accessPointName);
 	// initialize the call to rest API through the access point name
-	$scope.accessPointimage="/img/Early_icon_Sensor_Upgrade.png";
-	$scope.sensorPointimage="/img/sensor_icon.png";
+	$scope.accessPointimage = "/img/Early_icon_Sensor_Upgrade.png";
+	$scope.sensorPointimage = "/img/sensor_icon.png";
 	$scope.sensorPoints = [ {
 		name : "Sensor I",
-		overAllStatus : "Green"
+		overAllStatus : "Green",
+		LastTempUTC : "04:00",
+		LastTempValueC : 32
 	}, {
 		name : "Sensor II",
-		overAllStatus : "Green"
+		overAllStatus : "Green",
+		LastTempUTC : "02:00",
+		LastTempValueC : 30
 	}, {
 		name : "Sensor III",
-		overAllStatus : "Green"
+		overAllStatus : "Green",
+		LastTempUTC : "03:00",
+		LastTempValueC : 31.3
 	}, {
 		name : "Sensor IV",
-		overAllStatus : "Green"
+		overAllStatus : "Green",
+		LastTempUTC : "04:08",
+		LastTempValueC : 32
 	}, {
 		name : "Sensor V",
-		overAllStatus : "Green"
+		overAllStatus : "Green",
+		LastTempUTC : "04:03",
+		LastTempValueC : 32
 	} ];
 	
 	
@@ -28,6 +38,29 @@ var BNAccessSelectedAPointController = function($scope, accessPointService,
 		$scope.accessPointService.sensorPointName = sensorPointName;
 		$log.info($scope.sensorPointService.sensorPointName);
 	}
+
+	var dataArray = [];
+	for (var i = 0; i < $scope.sensorPoints.length; i++) {
+		dataArray.push({
+			"label" : $scope.sensorPoints[i].name,
+			"value" : $scope.sensorPoints[i].LastTempValueC,
+			dataFormat: 'json',
+			"tooltext" : "Sensor: "+$scope.sensorPoints[i].name+ "{br} Last Time Recorded "+$scope.sensorPoints[i].LastTempUTC
+		});
+	}
+
+	$scope.dataSourceForSensors = {
+		chart : {
+			caption : $scope.accessPointService.accessPointName,
+			subCaption : 'Last Temprature recorded for Sensors ',
+			xAxisName : "Sensors in "+$scope.accessPointService.accessPointName,
+			yAxisName : "Temperature",
+			numberSuffix : "F",
+			showValues : "0",
+			theme : "fint"
+		},
+		data : dataArray
+	};
 }
 
 BNAccessHome.controller("BNAccessSelectedAPointController",
